a25c6d20185d9d65bbdeaed033760661f5b22927
MakoDoli <makodolidze@hotmail.com>
2024-07-15 23:08:07 +0400

    configure eslint

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const app = express();
 
 // middlewares
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from frightful middleware 👋...');
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+/* eslint-disable prefer-template */
+const dotenv = require('dotenv');
+
+dotenv.config();
 const app = require('./app');
+// Load environment variables from.env file
+
+console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
