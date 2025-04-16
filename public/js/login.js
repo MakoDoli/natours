62c6ee5a1a6f7b739691cc2ac4e502ca08db07f8
MakoDoli <makodolidze@hotmail.com>
2025-04-16 15:46:16 +0400

    add alerts

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,6 @@
 /*eslint-disable*/
 import axios from 'axios';
+import { showAlert } from './alerts';
 export const login = async (email, password) => {
   try {
     const res = await axios({
@@ -9,13 +10,13 @@ export const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      alert('Logged in successfully');
+      showAlert('success', 'Logged in successfully');
       window.setTimeout(() => {
         location.assign('/');
       }, 500);
     }
   } catch (err) {
     console.error(err);
-    alert('Invalid email or password. Please try again.');
+    showAlert('error', 'Invalid email or password. Please try again.');
   }
 };
